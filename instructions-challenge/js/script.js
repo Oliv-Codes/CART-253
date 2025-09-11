740935d0121418bf9cc399ecd740457a4efc4939
Oliv-Codes <oliviaho6103261@gmail.com>
2025-09-11 12:36:18 -0700

    First attempt at beach

--- instructions-challenge/js/script.js
+++ instructions-challenge/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Title of Project
- * Author Name
+ * Beach
+ * Olivia, Xueyi
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Beautiful Beach
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * create the canvas
 */
 function setup() {
-
+createCanvas(640,480)
 }
 
 
+
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * calling functions to draw
 */
 function draw() {
+    drawSky();
+    drawSun();
+    drawOcean();
+    drawSand();
+}
 
+function drawSky() {
+   backgroud(148,203,246); 
+}
+
+function drawOcean() {
+    fill(111,238,216);
+    noStroke();
+    rect(640,300);
+}
+function drawSand() {
+    fill(200,190,120);
+    noStroke();
+    rect(240,100);
+}
+function drawSun() {
+    fill(255,255,255);
+    noStroke();
+    circle(80,80,130,300);
 }
