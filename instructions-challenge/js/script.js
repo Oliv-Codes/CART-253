bcff4c2711ea07c5c1526513dacba6f527587b0d
Oliv-Codes <oliviaho6103261@gmail.com>
2025-09-11 13:48:29 -0700

    added fish

--- instructions-challenge/js/script.js
+++ instructions-challenge/js/script.js
@@ -23,9 +23,11 @@ angleMode(DEGREES)
 function draw() {
     drawSky();
     drawSun();
+    drawFish();
     drawOcean();
     drawSand();
     drawBird();
+    drawUmbrella();
 }
 
 function drawSky() {
@@ -34,7 +36,7 @@ function drawSky() {
 
 function drawOcean() {
     push();
-    fill(111,238,216);
+    fill(111,238,216,150);
     noStroke();
     rect(0,300,640,300);
     pop();
@@ -50,16 +52,33 @@ function drawSun() {
     push();
     fill(255,230,0);
     noStroke();
-    circle(120,110,130,300);
+    circle(120,90,130,300);
     pop();
 }
 function drawBird() {
     push();
-    noFill()
-    stroke(0,0,0)
-    arc(300,150,80,30,180,0)
-    arc(380,150,80,30,180,0)
-    arc(420,200,60,20,180,0)
-    arc(480,200,60,20,180,0)
+    noFill();
+    stroke(0,0,0);
+    arc(300,150,80,30,180,0);
+    arc(380,150,80,30,180,0);
+    arc(420,200,60,20,180,0);
+    arc(480,200,60,20,180,0);
     pop();
+}
+function drawUmbrella() {
+    push();
+    fill(235,14,29)
+    arc(205,250,300,100,180,0)
+    fill(105,70,60);
+    noStroke();
+    rect(200,250,15,200);
+    pop();
+}
+function drawFish () {
+    push();
+    fill(235,138,225)
+    noStroke();
+    circle(390,350,40)
+    arc(420,350,30,30,90,0);
+    
 }
