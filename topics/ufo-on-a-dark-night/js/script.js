a451c443a9d3ad8a79eaf14d6936a2d506aa1d15
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2025-09-18 03:51:25 -0400

    used variables to make UFO fly away

--- topics/ufo-on-a-dark-night/js/script.js
+++ topics/ufo-on-a-dark-night/js/script.js
@@ -33,8 +33,21 @@ function setup() {
  * Displays a UFO
 */
 function draw() {
+    // Dawn
+    skyShade = skyShade + 1
     // Display the sky
     background(skyShade);
+
+    //Move UFO
+    ufo.x = ufo.x + .5;
+    ufo.y = ufo.y - 2;
+
+    //Change fill
+    ufo.fill - ufo.fill * 0.995;
+
+    //Change shape
+    ufo.width = ufo.width / 1.005;
+    ufo.height = ufo. height / 1.005;
 
     // Draw the UFO based on its properties
     push();
