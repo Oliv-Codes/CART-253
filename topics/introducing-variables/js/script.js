fb9db4ef6e173dedfce08aa4ea25af3e499fbeae
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2025-09-18 02:40:28 -0400

    Used variables to make the program change

--- topics/introducing-variables/js/script.js
+++ topics/introducing-variables/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Title of Project
- * Author Name
+ * Olivia
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -9,16 +9,24 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creaing canvas
 */
 function setup() {
+    createCanvas (640,640)
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw circle in center of canvas
 */
 function draw() {
+    background(0)
+
+    // Draw circle
+    push();
+    fill(255,255,0);
+    noStroke();
+    ellipse(mouseX, mouseY, 100 , 100);
 
 }
