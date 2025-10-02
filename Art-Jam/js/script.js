c3de19cccdf3c62f09775825f9b833a505cba8f0
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2025-10-02 00:14:36 -0400

    base complete + background variable added

--- Art-Jam/js/script.js
+++ Art-Jam/js/script.js
@@ -17,10 +17,22 @@ createCanvas(480, 640);
 //Varables
 
 //colours
+let sky = {
+    r: 125,
+    g: 105,
+    b: 100,
+}
+
 let hair = {
-r: 45,
-g: 30,
-b: 20
+    r: 35,
+    g: 30,
+    b: 20,
+}
+
+let eye = {
+    r: 255,
+    g: 255,
+    b: 255,
 }
 
 /**
@@ -30,14 +42,19 @@ function draw() {
     drawSky();
     drawHead();
     drawHair()
+    drawShirt();
     drawNeck();
+    drawBlu();
     drawJaw();
     drawBang();
-    
+    drawEye();    
 }
 
 function drawSky() {
-   background(125,105,100); 
+    sky.r = sky.r  -2
+    sky.g = sky.g  -2
+    sky.b = sky.b  -2
+     background(sky.r, sky.g, sky.b);
 }
 
 /**
@@ -55,7 +72,7 @@ function drawJaw() {
     push();
     fill(215,175,120);
     noStroke();
-     square(140, 250, 200, 50);;
+    square(140, 250, 200, 50);;
     pop();
 }
 
@@ -63,7 +80,15 @@ function drawNeck() {
     push();
     fill(200,135,105);
     noStroke();
-     square(180, 390, 120, 50);;
+     square(183, 390, 115, 43);;
+    pop();
+}
+
+function drawShirt() {
+    push();
+    fill(80,115,80);
+    noStroke();
+    square(90, 470, 300, 50);;
     pop();
 }
 
@@ -71,7 +96,8 @@ function drawBlu() {
     push();
     fill(0,50,255);
     noStroke();
-    quad(350, 340, 250, 285, 250, 230, 350, 250);
+    rect(120, 230, 20, 200);
+    rect(337, 230, 15, 190);
     pop();
 }
 function drawBang() {
@@ -96,5 +122,14 @@ function drawHair() {
     ellipse(140,400,50,200);
     ellipse(330,400,50,200);
     rect(140, 300, 200, 200);
+    pop();
+}
+
+function drawEye() {
+    push();
+    fill(eye.r,eye.g,eye.b);
+    noStroke();
+    bezier(170, 360, 240, 410, 240, 305, 170, 360);
+    bezier(310, 360, 240, 410, 240, 305, 310, 360);
     pop();
 }
