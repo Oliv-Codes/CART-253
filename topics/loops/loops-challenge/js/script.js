f653593e174e49b47a4fd48156e29422c4d6af9c
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2025-11-06 12:55:55 -0800

    step 4 completed

--- topics/loops/loops-challenge/js/script.js
+++ topics/loops/loops-challenge/js/script.js
@@ -24,19 +24,25 @@ function setup() {
 function draw() {
     background("pink");
 
+    for (let i = 0; i <= width; ++i) {
+        let hue = map(i, 0, width, 0, 255)
+        push()
+        colorMode(HSB)
+        stroke(hue, 255, 255)
+        line(i, 0, i, height)
+        pop()
+    }
+
     let strokeColor = 0;
-
     let lineX = 0;
-
-    let lineY = 0;
-
     while (strokeColor <= 250) {
         stroke(strokeColor)
         strokeColor = strokeColor + 25
         line(lineX, 0, lineX, height)
         lineX = lineX + 50
     }
-
+    
+    let lineY = 0;
     strokeColor = 0;
     while (strokeColor <= 250) {
         stroke(strokeColor)
@@ -44,6 +50,7 @@ function draw() {
         line(0, lineY, width, lineY)
         lineY = lineY + 50
     }
+
     
     // stroke(0);
     // line(0, 0, 0, height);
