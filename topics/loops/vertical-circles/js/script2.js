e52fa179c31c92bd44e830de212299e49d911005
Oliv-Codes <230441967+Oliv-Codes@users.noreply.github.com>
2025-11-05 20:12:34 -0500

    started loops

---
+++ topics/loops/vertical-circles/js/script2.js
@@ -0,0 +1,12 @@
+while (condition) {
+    //Do this
+}
+//We get to here when condition is false
+
+
+while (circles no reach bottom) {
+    //Do this
+    Draw a circles
+    move position down
+}
+//We get to here when condition is false
